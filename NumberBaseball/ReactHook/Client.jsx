7fe9cc0950b30e0ab2abeddb46f4e0c63bb9b695
semnil5202 <semnil5202@gmail.com>
2022-10-12 02:23:45 +0900

    숫자야구 리액트 훅스

---
+++ NumberBaseball/ReactHook/Client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import NumberBaseball from "./NumberBaseball";
+
+ReactDom.render(<NumberBaseball />, document.querySelector("#root"));
