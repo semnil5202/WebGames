e37b2105839110f41cf058db49047793866df786
semnil5202 <semnil5202@gmail.com>
2022-10-13 23:04:21 +0900

    Client.jsx createRoot

--- .init/Client.jsx
+++ .init/Client.jsx
@@ -1,5 +1,5 @@
 import React from "react"; // const React = require('react');
-import ReactDom from "react-dom"; // const ReactDom = require('react-dom');
+import ReactDom from "react-dom/client"; // const ReactDom = require('react-dom');
 import ProjectName from "./ProjectName"; // const ProjectName = require('./ProjectName')
 
-ReactDom.render(<ProjectName />, document.querySelector("#root"));
+ReactDom.createRoot(document.querySelector("#root")).render(<ProjectName />);
